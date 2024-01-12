c5cff2d1f0c2d2a759fc444dfe69cfc48e773aec
Samuel Aliyari <samuelaliyari@outlook.de>
2024-01-12 16:23:55 +0100

    404 added

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ app.use(express.static("public"));
 
 app.get("/", (_, res) => res.sendFile(__dirname + "/public/pages/index.html"));
 
+app.use((req, res) => res.sendFile(__dirname + "/public/pages/404.html"))
 const PORT = process.env.PORT || 1818;
 app.listen(PORT, () => console.log("App running @ port: " + PORT));
